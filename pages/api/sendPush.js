5fe31d3eb1176972d99a595389c66faff2acdb40
JHD <44101959+jhd71@users.noreply.github.com>
2025-02-08 04:11:24 +0100

    Update sendPush.js

--- pages/api/sendPush.js
+++ pages/api/sendPush.js
@@ -12,7 +12,7 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
