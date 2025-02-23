13429e0b30e09b6c453a8b2523d51618fcf49134
JHD <44101959+jhd71@users.noreply.github.com>
2025-02-23 23:23:00 +0100

    Update firebase-messaging-sw.js

--- public_html/firebase-messaging-sw.js
+++ public_html/firebase-messaging-sw.js
@@ -14,11 +14,12 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+    console.log('Message reçu en arrière-plan :', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
         icon: '/images/INFOS-192.png'
     };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
