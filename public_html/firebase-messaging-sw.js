2acc21a5e2cef3a4b5b2a1c0d8deffc2e1abae1c
JHD <44101959+jhd71@users.noreply.github.com>
2025-02-24 03:48:54 +0100

    Update firebase-messaging-sw.js

--- public_html/firebase-messaging-sw.js
+++ public_html/firebase-messaging-sw.js
@@ -1,25 +1,21 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDGC0jBKzFYpv2dSsgrKAZlzMirTjqKjpk",
-    authDomain: "jhd71-fbe56.firebaseapp.com",
-    projectId: "jhd71-fbe56",
-    storageBucket: "jhd71-fbe56.firebasestorage.app",
-    messagingSenderId: "669167096860",
-    appId: "1:669167096860:web:d46d695cd8a56571ee3bd9",
-    measurementId: "G-E61V8DTJ2W"
-});
+// Charger la configuration Firebase depuis le Service Worker Registration
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.firebaseConfig) {
+        firebase.initializeApp(event.data.firebaseConfig);
+        const messaging = firebase.messaging();
 
-const messaging = firebase.messaging();
+        messaging.onBackgroundMessage((payload) => {
+            console.log('Message reçu en arrière-plan :', payload);
+            const notificationTitle = payload.notification.title;
+            const notificationOptions = {
+                body: payload.notification.body,
+                icon: '/images/INFOS-192.png'
+            };
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('Message reçu en arrière-plan :', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/images/INFOS-192.png'
-    };
-
-    return self.registration.showNotification(notificationTitle, notificationOptions);
+            return self.registration.showNotification(notificationTitle, notificationOptions);
+        });
+    }
 });
