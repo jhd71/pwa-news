2b5b8cce009d19a058ab06cd3573196862313ab0
JHD <44101959+jhd71@users.noreply.github.com>
2025-03-08 07:45:58 +0100

    Update getNationalNews.js

--- api/getNationalNews.js
+++ api/getNationalNews.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     const feeds = [
       { name: 'BFMTV', url: 'https://www.bfmtv.com/rss/news-24-7/', max: 3 },
       { name: 'France Info', url: 'https://www.francetvinfo.fr/titres.rss', max: 3 },
-      { name: 'Football Orange', url: 'https://cdn.feedcontrol.net/8812/14711-qEEkvXNtTtNKV.xml', max: 3 }
+      { name: 'Football', url: 'https://cdn.feedcontrol.net/8812/14712-TRKjBFrPv0Doj.xml', max: 3 }
     ];
 
     const allArticles = [];
