4d6da7efc583e48a2bd58902ff0e38299a265c40
JHD <herve71@free.fr>
2025-04-18 03:56:11 +0200

    Update send-important-notification.js

--- api/send-important-notification.js
+++ api/send-important-notification.js
@@ -4,7 +4,7 @@ import webpush from 'web-push';
 
 // Initialiser le client Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Configurer les clés VAPID pour les notifications Web Push
