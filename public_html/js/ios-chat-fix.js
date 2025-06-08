4232701d26e8b8771e7da3b4d64eb214015afa96
JHD <herve71@free.fr>
2025-06-08 07:59:43 +0200

    ios

--- public_html/js/ios-chat-fix.js
+++ public_html/js/ios-chat-fix.js
@@ -2,9 +2,7 @@
 
 (function() {
     // Ne s'exécuter que sur iOS
-    const isIOS = (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) && 
-              !(/Android/.test(navigator.userAgent)) && 
-              !(/Chrome/.test(navigator.userAgent) && /Mobile/.test(navigator.userAgent));
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     if (!isIOS) return;
     
     console.log("iOS détecté - Activation des correctifs améliorés pour le chat");
