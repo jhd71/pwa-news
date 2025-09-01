d8a70bfca406335d993290ca5407d1bfd8af5edd
JHD <44101959+jhd71@users.noreply.github.com>
2025-09-01 05:38:26 +0200

    Update getNationalNews.js

--- api/getNationalNews.js
+++ api/getNationalNews.js
@@ -32,7 +32,8 @@ export default async function handler(req, res) {
   { name: 'France Info', url: 'https://www.francetvinfo.fr/titres.rss', max: 1 },
   { name: 'BFMTV', url: 'https://www.bfmtv.com/rss/news-24-7/', max: 1 },
   { name: 'France 3 Bourgogne', url: 'https://france3-regions.francetvinfo.fr/bourgogne-franche-comte/rss', max: 2 }, // 🆕 RÉGIONAL !
-  { name: 'CNEWS', url: 'https://www.cnews.fr/rss.xml', max: 1 }
+  { name: 'CNEWS', url: 'https://www.cnews.fr/rss.xml', max: 1 },
+  { name: 'Montceau News', url: 'https://montceau-news.com/feed/', max: 2 }
 ];
     
     let articles = [];
