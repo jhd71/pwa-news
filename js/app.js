0fc1c537715b2638a2738169f1620ae35267a1a5
jhd71 <herve71@free.fr>
2025-12-17 02:21:05 +0100

    33

--- js/app.js
+++ js/app.js
@@ -191,18 +191,9 @@ function renderNewsSlider(articles) {
             ${articles.map((article, index) => `
                 <div class="news-slide">
                     <a href="${article.link}" target="_blank" class="news-item fade-in" style="animation-delay: ${index * 0.1}s">
-                        <div class="news-item-image">
-                            ${article.image ? `
-                                <img src="${article.image}" alt="" loading="lazy" onerror="this.onerror=null; this.style.display='none'; this.parentElement.innerHTML='<span class=news-placeholder>${getSourceIcon(article.source)}</span>';">
-                            ` : `
-                                <span class="news-placeholder">${getSourceIcon(article.source)}</span>
-                            `}
-                        </div>
-                        <div class="news-item-content">
-                            <div class="news-item-source">${getSourceIcon(article.source)} ${article.source}</div>
-                            <div class="news-item-title">${article.title}</div>
-                            <div class="news-item-date">${formatDate(article.date)}</div>
-                        </div>
+                        <div class="news-item-source">${getSourceIcon(article.source)} ${article.source}</div>
+                        <div class="news-item-title">${article.title}</div>
+                        <div class="news-item-date">${formatDate(article.date)}</div>
                     </a>
                 </div>
             `).join('')}
