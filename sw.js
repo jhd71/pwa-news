69d8170ae7ff0ee6c47732abeca4250bb502935b
jhd71 <herve71@free.fr>
2025-12-22 01:48:35 +0100

    66

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // ACTU & MÉDIA - Service Worker v2
 // ============================================
 
-const CACHE_NAME = 'actu-media-v17';
+const CACHE_NAME = 'actu-media-v18';
 
 const STATIC_ASSETS = [
     '/',
