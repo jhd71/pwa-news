830e38e1643e27cb4cc0bf0a245f66b3775a3e7f
jhd71 <herve71@free.fr>
2026-08-19 03:46:06 +0200

    v12

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ============================================
-// ACTU & MÉDIA - Service Worker v58
+// ACTU & MÉDIA - Service Worker v59
 // ============================================
 
-const CACHE_NAME = 'actu-media-v58';
+const CACHE_NAME = 'actu-media-v59';
 
 // Assets statiques à mettre en cache à l'installation
 // Ne PAS inclure les pages admin (toujours besoin de données fraîches)
@@ -55,7 +55,7 @@ const NETWORK_ONLY = [
 
 // Installation
 self.addEventListener('install', event => {
-    console.log('📦 SW v58: Installation');
+    console.log('📦 SW v59: Installation');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
@@ -75,7 +75,7 @@ self.addEventListener('install', event => {
 
 // Activation - supprime les anciens caches
 self.addEventListener('activate', event => {
-    console.log('🚀 SW v58: Activation');
+    console.log('🚀 SW v59: Activation');
     event.waitUntil(
         caches.keys().then(keys => {
             return Promise.all(
