f3077ed72161517c7cfb16e80559bb17ca1bdc8f
jhd71 <herve71@free.fr>
2026-08-23 23:59:48 +0200

    22

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // ACTU & MÉDIA - Service Worker v81
 // ============================================
 
-const CACHE_NAME = 'actu-media-v92';
+const CACHE_NAME = 'actu-media-v93';
 
 // Assets statiques à mettre en cache à l'installation
 // Ne PAS inclure les pages admin (toujours besoin de données fraîches)
