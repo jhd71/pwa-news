bf46eb59dba9a7489f8ab12760620910d3259c9f
jhd71 <herve71@free.fr>
2026-09-24 08:46:33 +0200

    66

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Les messages de la console la reprennent automatiquement.
 // ============================================
 
-const CACHE_NAME = 'actu-media-v126';
+const CACHE_NAME = 'actu-media-v127';
 const VERSION = CACHE_NAME.split('-').pop();
 
 // Assets statiques à mettre en cache à l'installation
